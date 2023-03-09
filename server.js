bde05102a64b04e223f48cfcd0df2d7659ddb33a
beastbybirth <113232311+beastbybirth@users.noreply.github.com>
2023-03-10 03:09:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8081;
 
-app.get('/', (req, res) => {
-    res.send('hello world');
+app.get('/', async (req, res) => {
+  const username = req.query.username || 'beast';
+  try {
+    const result = await axios.get(
+      `https://api.github.com/users/${username}/repos`
+    );
+    const repos = result.data
+      .map((repo) => ({
+        name: repo.name,
+        url: repo.html_url,
+        description: repo.description,
+        stars: repo.stargazers_count
+      }))
+      .sort((a, b) => b.stars - a.stars);
+
+    res.send(repos);
+  } catch (error) {
+    res.status(400).send('Error while getting list of repositories');
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`);
+  console.log(`server started on port ${PORT}`);
 });
